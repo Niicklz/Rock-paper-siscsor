a1ff076f977108c1243dded1da86eae84725b260
Niicklz <77895883+Niicklz@users.noreply.github.com>
2022-09-04 19:40:02 -0400

    mejoramos todo
    y ta mas cortito

--- script.js
+++ script.js
@@ -1,22 +1,60 @@
 console.log("Ahi te voy san pedro");
 const cpuCount = document.querySelector("#com-score");
 const userCount = document.querySelector("#user-score");
-
+const buttons = Array.from (document.querySelectorAll("[data-option]"));
 const resultado = document.querySelector("h1");
-const rockButton = document.querySelector("#rock");
-const paperButton = document.querySelector("#paper");
-const scissorButton = document.querySelector("#scissor");
-const comRockImg = document.querySelector("#com-hand-rock");
-const userRockImg = document.querySelector("#user-hand-rock");
-const comPaperImg = document.querySelector("#com-hand-paper");
-const userPaperImg = document.querySelector("#user-hand-paper");
-const comScissorImg = document.querySelector("#com-hand-scissor");
-const userScissorImg = document.querySelector("#user-hand-scissor");
-let options = ["rock", "paper", "scissor"];
+const userOptionImg = document.querySelector("#user-option-img");
+const comOptionImg = document.querySelector("#com-option-img");
+const imgSelection = [userOptionImg, comOptionImg];
+const UserNameValue = document.querySelector("#user-name");
+let userName = prompt("Cual es tu nombre?");
+
+while (userName.trim() === "" ) {
+  userName = prompt("Cual es tu nombre?");
+}
+
+UserNameValue.innerText = userName;
+
+function showSelectionsImg (userImg, cpuImg) {
+  userOptionImg.setAttribute("src", `/${userImg}.png`)
+  comOptionImg.setAttribute("src", `/${cpuImg}.png`)
+  imgSelection[0].classList.remove("inactive")
+  imgSelection[1].classList.remove("inactive")
+}
+
+
+
+
+
+
+const OPTIONS_NAMES = {
+  ROCK: "rock",
+  PAPER: "paper",
+  SCISSOR: "scissor"
+}
+
 let cpuScore = 0;
 let userScore = 0;
 
-function winner () {
+const selections = [
+  {
+    value: OPTIONS_NAMES.ROCK,
+    lose: OPTIONS_NAMES.PAPER,
+    beat: OPTIONS_NAMES.SCISSOR
+  },
+  {
+    value: OPTIONS_NAMES.PAPER,
+    lose: OPTIONS_NAMES.SCISSOR,
+    beat: OPTIONS_NAMES.ROCK
+  },
+  {
+    value: OPTIONS_NAMES.SCISSOR,
+    lose: OPTIONS_NAMES.ROCK,
+    beat: OPTIONS_NAMES.PAPER
+  }
+]
+
+function win () {
     userScore++;
     userCount.innerText = userScore;
 }
@@ -27,91 +65,67 @@ function lose () {
 
 
 function getCpuChoice() {
-  const choices = ["rock", "paper", "scissor"];
-  const randomNumber = Math.floor(Math.random() * 3);
-  return choices[randomNumber];
+  
+  const randomNumber = Math.floor(Math.random() * selections.length);
+  return selections[randomNumber];
 }
 
-rockButton.addEventListener("click", () => {
-  play(options[0]);
-});
-paperButton.addEventListener("click", () => {
-  play(options[1]);
-});
-scissorButton.addEventListener("click", () => {
-  play(options[2]);
-});
+buttons.forEach((button,index)  => {
+  button.addEventListener("click",() => {
+    play(selections[index])
+  }  )
+})
+
+
+
+function resultadoGame(winner,WinnerGame ) {
+  if (winner === null) {
+    resultado.innerText = `Es un empate con ${WinnerGame}`
+    return
+  }
+ 
+  resultado.innerText = `El ganador es ${winner} con ${WinnerGame}`
+
+  
+}
 
 function play(userSelection) {
   const cpuChoice = getCpuChoice();
-  comRockImg.classList.add("inactive");
-  userRockImg.classList.add("inactive");
-  comPaperImg.classList.add("inactive");
-  userPaperImg.classList.add("inactive");
-  comScissorImg.classList.add("inactive");
-  userScissorImg.classList.add("inactive");
+  imgSelection.forEach(img => {
+    img.classList.add("inactive")
+   })
 
-  if (userSelection === "rock" && cpuChoice === "scissor") {
-    console.log("Ha ganado el usuario");
-    resultado.innerText = "Ha ganado el usuario! con " + userSelection + "!";
-    userRockImg.classList.remove("inactive");
-    comScissorImg.classList.remove("inactive");
-    winner();
+  const isUserWins = userSelection.value === cpuChoice.lose;
+  console.log(isUserWins);
+  const isDraw = userSelection.value === cpuChoice.value;
+
+  if(isDraw) {
+    console.log("empate con " + userSelection.value + " " + cpuChoice.value)
+    resultadoGame(null, userSelection.value);
+
+    
   }
+  if (isUserWins) {
+    console.log("Gano el usuario con " + userSelection.value + " y perdio el cpu con " + cpuChoice.value);
+    resultadoGame(userName, userSelection.value);
 
-  if (userSelection === "paper" && cpuChoice === "rock") {
-    console.log("Ha ganado el usuario");
-    resultado.innerText = "Ha ganado el usuario! con " + userSelection + "!";
-    userPaperImg.classList.remove("inactive");
-    comRockImg.classList.remove("inactive");
-    winner();
+
+    win();
+   
   }
+  if (!isUserWins && !isDraw) {
+   console.log("perdio el usuario con " + userSelection.value + " y gano el CPU con " + cpuChoice.value)
+   resultadoGame(`CPU`, cpuChoice.value);
 
-  if (userSelection === "scissor" && cpuChoice === "paper") {
-    console.log("Ha ganado el usuario");
-    resultado.innerText = "Ha ganado el usuario! con " + userSelection + "!";
-    userScissorImg.classList.remove("inactive");
-    comPaperImg.classList.remove("inactive");
-    winner();
+   lose();
   }
 
-  if (userSelection === "rock" && cpuChoice === "rock") {
-    console.log("Es un empate");
-    resultado.innerText = "Empate con " + userSelection + "!";
-    userRockImg.classList.remove("inactive");
-    comRockImg.classList.remove("inactive");
-  }
-  if (userSelection === "paper" && cpuChoice === "paper") {
-    console.log("Es un empate");
-    resultado.innerText = "Empate con " + userSelection + "!";
-    userPaperImg.classList.remove("inactive");
-    comPaperImg.classList.remove("inactive");
-  }
-  if (userSelection === "scissor" && cpuChoice === "scissor") {
-    console.log("Es un empate");
-    resultado.innerText = "Empate con " + userSelection + "!";
-    userScissorImg.classList.remove("inactive");
-    comScissorImg.classList.remove("inactive");
-  }
-  if (userSelection === "rock" && cpuChoice === "paper") {
-    console.log("Ha ganado el CPU");
-    resultado.innerText = "Ha ganado el CPU con " + userSelection + "!";
-    userRockImg.classList.remove("inactive");
-    comPaperImg.classList.remove("inactive");
-    lose();
-  }
-  if (userSelection === "paper" && cpuChoice === "scissor") {
-    console.log("Ha ganado el CPU");
-    resultado.innerText = "Ha ganado el CPU con " + userSelection + "!";
-    userPaperImg.classList.remove("inactive");
-    comScissorImg.classList.remove("inactive");
-    lose();
-  }
-  if (userSelection === "scissor" && cpuChoice === "rock") {
-    console.log("Ha ganado el CPU");
-    resultado.innerText = "Ha ganado el CPU con " + userSelection + "!";
-    userScissorImg.classList.remove("inactive");
-    comRockImg.classList.remove("inactive");
-    lose();
-  }
+  showSelectionsImg(userSelection.value, cpuChoice.value);
+  
+
+
 }
+
+console.log(buttons)
+
+console.log("Ya sirve denuevo");
